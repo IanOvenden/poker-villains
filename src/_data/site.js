b7935984f0c4d4ab61ec565eda79030d8b2c7abb
Ian Ovenden <ian.ovenden@pega.com>
2022-10-04 14:16:31 +0100

    First templates

---
+++ src/_data/site.js
@@ -0,0 +1,11 @@
+module.exports = {
+	"title": "Poker Villains",
+	"description": "Sunday night poker league management",
+	"url": "poker-villains.app",
+	"author": "Ian Ovenden",
+	"meta_data": {
+		"twitter": "@ianovenden",
+		"opengraph_default": "/static/opengraph-default.jpg"
+	},
+	"env": process.env.ELEVENTY_ENV === 'production'
+}
